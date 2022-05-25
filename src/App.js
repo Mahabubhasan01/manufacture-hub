666d7ed5f4ae11ce4c556d968fd329e45819373a
Mahabubhasan01 <mahabubhasaan@outlook.com>
2022-05-25 13:55:41 +0600

    privet and public route set done into app js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './Pages/Components/HomePage/Navbar/Navbar';
+import { PublicRoute } from './Pages/Routes/PublicRoutes/PublicRoutes';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
+      <Routes>
+        {PublicRoute.map(({path,Component},index)=>(
+          <Route key={index} path={path} component={<Component></Component>}></Route>
+        ))}
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Routes/PublicRoutes/PublicRoutes.js
@@ -0,0 +1,5 @@
+import Home from '../../Components/HomePage/Home';
+
+export const PublicRoute = [
+    { path: "/", name: "Home", Component: Home },
+]

63210bb97d3ddc682c9592324566d1696b63b385
Mahabubhasan01 <mahabubhasaan@outlook.com>
2022-05-26 23:36:58 +0600

    spiner add

---
+++ src/Pages/Shared/Spiner/Spiner.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Spiner = () => {
+  return (
+    <div className="spn">
+      <div class="ring">
+        Loading
+        <span></span>
+      </div>
+    </div>
+  );
+};
+
+export default Spiner;
